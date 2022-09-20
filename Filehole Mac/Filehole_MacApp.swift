3ad3c5830a3723157dbc4ddafb9c0a1408398b49
Michael Eisemann <me@michaeleisemann.com>
2022-09-20 18:02:22 -0400

    Initial Commit

---
+++ Filehole Mac/Filehole_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Filehole_MacApp.swift
+//  Filehole Mac
+//
+//  Created by Michael Eisemann on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Filehole_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
